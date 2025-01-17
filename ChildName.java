c92c6a700380b97ea06c8f483e56e5ab4d3f5be4
Artsiom <multiversefly@gmail.com>
2025-01-17 15:28:39 +0300

    Initial commit of ChildName.java

---
+++ ChildName.java
@@ -0,0 +1,24 @@
+public class ChildName extends Name {
+    private String operator;
+
+    public ChildName(int hours, int minutes, int seconds, String operator) {
+        super(hours, minutes, seconds);
+        this.operator = operator;
+    }
+
+    public boolean isPrivilegedTime() {
+        return getHours() >= 0 && getHours() < 8;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", operator: " + operator + ", privileged time: " + isPrivilegedTime();
+    }
+     public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
